92732ac6e7dc56bf400669ca92a731389b381bca
derrick b <dbozich1@gmail.com>
2018-07-21 20:08:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,8 +2,9 @@ class PigLatinizer
 
 
   def piglatinize(input)
-    x = (input.split(" ").length == 1) ? piglatinize_word(input) : piglatinize_sentence(input)
     binding.pry
+    x = (input.split(" ").length == 1) ? piglatinize_word(input) : piglatinize_sentence(input)
+
     puts x
     x
   end
